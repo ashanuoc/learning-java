3ea3a687c8c2b7fd94eea6dc136e4db86c507b9d
ashanuoc <59000312+ashanuoc@users.noreply.github.com>
2024-11-13 06:26:36 -0500

    Modified to print sum

--- Exercises/Exercises/src/PrintEvenNumbersWhile.java
+++ Exercises/Exercises/src/PrintEvenNumbersWhile.java
@@ -2,12 +2,15 @@
 public class PrintEvenNumbersWhile {
     public static void main(String[] args) {
         int i = 2;
+        int sum = 0;
 
         while (i<=20){
             if (i%2==0){
                 System.out.println(i);
+                sum += i;
             }
             i++;
         }
+        System.out.println("Sum of even numbers = " + sum);
     }
 }
