4b3d37fc0112efa16130c4bfeb88aee1055491fc
ashanuoc <59000312+ashanuoc@users.noreply.github.com>
2024-11-13 06:17:43 -0500

    Password matcher

--- Exercises/Exercises/src/Password.java
+++ Exercises/Exercises/src/Password.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class Password {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        final String correctPassword = "test123";
+        String pw;
+
+        do {
+            System.out.print("Please enter your password: ");
+            pw = scanner.next();
+
+            if (!(correctPassword.equals(pw))){
+                System.out.println("Password invalid!");
+            }
+
+        } while (!(correctPassword.equals(pw)));
+
+        System.out.println("Your password matched!");
+
+        scanner.close();
+    }
 }
