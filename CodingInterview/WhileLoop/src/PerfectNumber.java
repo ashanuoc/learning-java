d5606f8b4e0cf9899c4ffcd592c32961ccf9a372
ashanuoc <59000312+ashanuoc@users.noreply.github.com>
2024-11-13 22:25:19 -0500

    perfect number

---
+++ CodingInterview/WhileLoop/src/PerfectNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        int i = 1;
+        int sum = 0;
+
+        while (i<=num/2){
+            if (num % i == 0){
+                sum += i;
+            }
+
+            i++;
+        }
+
+        if (num == sum){
+            System.out.println(num + " is a perfect number");
+        }
+        else {
+            System.out.println(num + " is not a perfect number");
+        }
+
+        scanner.close();
+    }
+}
