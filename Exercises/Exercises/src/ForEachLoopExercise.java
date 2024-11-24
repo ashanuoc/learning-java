f686f75bab4c812b71179253be387d3d1980ab65
ashanuoc <59000312+ashanuoc@users.noreply.github.com>
2024-11-23 19:56:10 -0500

    for each 1

--- Exercises/Exercises/src/ForEachLoopExercise.java
+++ Exercises/Exercises/src/ForEachLoopExercise.java
@@ -1,11 +1,24 @@
 public class ForEachLoopExercise {
     public static void main(String[] args) {
-        int [] arr = {1, 5, 20,60};
-        int sum = 0;
 
-        for (int i: arr){
-            sum += i;
+        char[] charArr = new char[] {'a', 's', 'd', 'a', 'e', 'a'};
+
+        int count = 0;
+
+        for (char c: charArr){
+            if (c == 'a'){
+                count++;
+            }
         }
-        System.out.println("Sum = " + sum);
+        System.out.println("Count = " + count);
+
+
+//        int [] arr = {1, 5, 20,60};
+//        int sum = 0;
+//
+//        for (int i: arr){
+//            sum += i;
+//        }
+//        System.out.println("Sum = " + sum);
     }
 }
