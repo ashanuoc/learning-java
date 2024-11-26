d076f8c71974e6d0e6b864989714f06288af9a0b
ashanuoc <59000312+ashanuoc@users.noreply.github.com>
2024-11-26 07:12:39 -0500

    total divisors count

---
+++ CodingInterview/WhileLoop/src/TotalNumberOfDivisors.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class TotalNumberOfDivisors {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number (>0): ");
+        int num = scanner.nextInt();
+
+        int i = 1;
+        int count = 0;
+
+        while (i<=num/2){
+            if (num % i == 0){
+                count++;
+            }
+
+            i++;
+        }
+
+        count +=1; // include the number itself
+        System.out.println("total number of divisors of number " + num + " is: " + count);
+
+
+        scanner.close();
+    }
+}
