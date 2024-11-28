5a44550c58ef64dcfdca0f930f8d75363a53b791
ashanuoc <59000312+ashanuoc@users.noreply.github.com>
2024-11-28 07:13:44 -0500

    UtilsArray

--- FirstProject/src/JavaUtilsArraysDemo.java
+++ FirstProject/src/JavaUtilsArraysDemo.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class JavaUtilsArraysDemo {
+    public static void main(String[] args) {
+        int[] myArray = {10, 4, 7, 0, 67, 3, 2, 9, 5};
+//        for (int i: myArray){
+//            System.out.println(i);
+//        }
+//        Arrays.sort(myArray);
+//        Arrays.sort(myArray,4, 6);
+        System.out.println(Arrays.toString(myArray));
+
+    }
 }
