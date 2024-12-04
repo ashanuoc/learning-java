96976d6affdeafeb5e1f16ae08eb6aadd83216f2
ashanuoc <59000312+ashanuoc@users.noreply.github.com>
2024-12-04 06:13:48 -0500

    optimal solution

--- Exercises/Exercises/src/LeetCodeContainerWithMostWater.java
+++ Exercises/Exercises/src/LeetCodeContainerWithMostWater.java
@@ -1,8 +1,30 @@
 public class LeetCodeContainerWithMostWater {
     public static void main(String[] args) {
         int[] height = {1,8,6,2,5,4,8,3,7};
+        int lp = 0, rp = height.length-1, maxWater = 0;
+        int w, l, currentWater;
+
+        while(lp < rp){
+            w = rp - lp;
+            l = Math.min(height[lp], height[rp]);
+            currentWater = l * w;
+
+            if (currentWater > maxWater){
+                maxWater = currentWater;
+            }
+
+            if (height[lp] < height[rp]){
+                lp++;
+            }
+            else{
+                rp--;
+            }
+        }
+        System.out.println("Max water= " + maxWater);
+
+
 //        Brute force
-        int maxArea = 0;
+        /*int maxArea = 0;
         int w, l, currentArea;
         int line1 =0, line2 = 0;
 
@@ -19,6 +41,6 @@ public class LeetCodeContainerWithMostWater {
             }
         }
         System.out.println("Max area = " + maxArea);
-        System.out.println("Container = line1: " + line1 + " and line2: " + line2);
+        System.out.println("Container = line1: " + line1 + " and line2: " + line2);*/
     }
 }
