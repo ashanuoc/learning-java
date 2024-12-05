3d19546440ce941a4f23ad33045531d096db94d6
ashanuoc <59000312+ashanuoc@users.noreply.github.com>
2024-12-05 06:24:34 -0500

    Initial commit

---
+++ Exercises/Exercises/src/ReplaceUnderscoresWithSpaces.java
@@ -0,0 +1,9 @@
+public class ReplaceUnderscoresWithSpaces {
+    public static void main(String[] args) {
+        String input = " Hello_World ";
+        String inputTrim = input.trim().replace(' ', '_');
+
+        System.out.println(inputTrim);
+
+    }
+}
