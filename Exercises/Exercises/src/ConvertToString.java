288f7481197f3cca7b462dc719e45aeb4a16f45f
ashanuoc <59000312+ashanuoc@users.noreply.github.com>
2024-12-05 06:40:58 -0500

    Initial commit

---
+++ Exercises/Exercises/src/ConvertToString.java
@@ -0,0 +1,9 @@
+public class ConvertToString {
+    public static void main(String[] args) {
+        int num = 42;
+        boolean flag = true;
+        char letter = 'A';
+
+        System.out.println(Integer.toString(num) + Boolean.toString(flag) + Character.toString(letter));
+    }
+}
