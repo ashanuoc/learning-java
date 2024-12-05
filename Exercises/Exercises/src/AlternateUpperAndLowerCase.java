3d5344cb9ed78fe01dd06b5ddf2a8dd3c2aea657
ashanuoc <59000312+ashanuoc@users.noreply.github.com>
2024-12-05 06:18:52 -0500

    Modified to ignore space

--- Exercises/Exercises/src/AlternateUpperAndLowerCase.java
+++ Exercises/Exercises/src/AlternateUpperAndLowerCase.java
@@ -1,13 +1,27 @@
 public class AlternateUpperAndLowerCase {
     public static void main(String[] args) {
         String input = "hello world";
+        int counter = 0;
+
         for (int i = 0; i < input.length(); i++){
-            if (i%2==0) {
-                System.out.print(input.toUpperCase().charAt(i));
+
+            char ch = input.charAt(i);
+
+            if (ch != ' '){
+                if (counter % 2==0) {
+                    System.out.print(Character.toUpperCase(ch));
+                }
+                else{
+                    System.out.print(ch);
+                }
+
+                counter++;
             }
-            else{
-                System.out.print(input.charAt(i));
+            else {
+                System.out.print(ch);
             }
+
+
         }
     }
 }
