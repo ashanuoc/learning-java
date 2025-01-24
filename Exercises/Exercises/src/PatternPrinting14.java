43aace3d2e031cff5a0ca9e30e994d6b4fb9cc31
ashanuoc <59000312+ashanuoc@users.noreply.github.com>
2025-01-23 23:28:03 -0500

    PatternPrinting14

---
+++ Exercises/Exercises/src/PatternPrinting14.java
@@ -0,0 +1,30 @@
+public class PatternPrinting14 {
+    public static void main(String[] args) {
+        for (int i=1; i<=5; i++){
+            for (int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+            for (int k=1; k<=10-2*i; k++) {
+                System.out.print(" ");
+            }
+            for (int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+        for (int i=4; i>=1; i--){
+            for (int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+            for (int k=1; k<=10-2*i; k++) {
+                System.out.print(" ");
+            }
+            for (int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
