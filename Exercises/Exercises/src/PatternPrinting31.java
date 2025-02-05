b73507bcdf71dea253ab5788a64a46991959279b
ashanuoc <59000312+ashanuoc@users.noreply.github.com>
2025-02-04 23:27:43 -0500

    PatternPrinting31-modified

--- Exercises/Exercises/src/PatternPrinting31.java
+++ Exercises/Exercises/src/PatternPrinting31.java
@@ -1,14 +1,28 @@
 public class PatternPrinting31 {
     public static void main(String[] args) {
         for (int i=1; i<=5; i++){
+//            for (int k=1; k<=5-i; k++){
+//                System.out.print(" ");
+//            }
+//            for (int j=1; j<=i; j++){
+//                System.out.print((char)(j+64)+" ");
+//            }
+//            for (int m=i; m>1; m--){
+//                System.out.print((char)(m+63)+" ");
+//            }
+//            System.out.println();
+            char ch='A';
             for (int k=1; k<=5-i; k++){
                 System.out.print(" ");
             }
             for (int j=1; j<=i; j++){
-                System.out.print((char)(j+64)+" ");
+                System.out.print(ch+" ");
+                ch++;
             }
+            ch-=2;
             for (int m=i; m>1; m--){
-                System.out.print((char)(m+63)+" ");
+                System.out.print(ch+" ");
+                ch--;
             }
             System.out.println();
         }
